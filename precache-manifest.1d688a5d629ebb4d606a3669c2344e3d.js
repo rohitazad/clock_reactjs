6ceb936581b645a3cc3592676f6666f2c1f21f0a
rohitazad <ram9953878727@gmail.com>
2019-02-27 10:22:10 +0530

    Updates

---
+++ precache-manifest.1d688a5d629ebb4d606a3669c2344e3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd9495500e9d31a8cefd",
+    "url": "/clock_reactjs/static/js/runtime~main.cd949550.js"
+  },
+  {
+    "revision": "9268af32ada67d49ad07",
+    "url": "/clock_reactjs/static/js/main.9268af32.chunk.js"
+  },
+  {
+    "revision": "bb1cfbc77e0591448a8c",
+    "url": "/clock_reactjs/static/js/2.bb1cfbc7.chunk.js"
+  },
+  {
+    "revision": "9268af32ada67d49ad07",
+    "url": "/clock_reactjs/static/css/main.7d286c7a.chunk.css"
+  },
+  {
+    "revision": "251281291c4bdcd9c38053c6f0bb500b",
+    "url": "/clock_reactjs/index.html"
+  }
+];
